b094583b65fee503a8335ef09b6eb7ad64cdb963
Victor Matia <vmatir@outlook.com>
2025-03-11 14:20:48 +0000

    Refactor Degrees and Radians into classes with operators.
    Converted Degrees and Radians from record structs to classes,
    implementing modular normalization, operator overloading, and enhanced
    formatting methods.
    
    Updated project version to 0.1.0 and enabled package generation during
    build.
    
    Signed-off-by: Victor Matia <vmatir@outlook.com>

--- Src/OpenCnC.Net.Math/Degrees.cs
+++ Src/OpenCnC.Net.Math/Degrees.cs
@@ -1,6 +1,35 @@
 namespace OpenCnC.Net.Math;
 
-public record struct Degrees(float Value)
+public class Degrees(float value)
 {
-    public Radians ToRadians() => new(Value * Constants.Pi / 180F);
+    // Keep between 0 and 360
+    private readonly float _value = (value % 360 + 360) % 360;
+
+    public static Degrees Zero => new(0F);
+
+    public static Degrees PerRadian => new(180F / Constants.Pi);
+
+    public Radians ToRadians() => _value * Radians.PerDegree;
+
+    public override string ToString()
+    {
+        var degrees = (int)_value;
+        var minutes = (int)((_value - degrees) * 60);
+        var seconds = (_value - degrees - minutes / 60F) * 3600;
+        return $"{degrees}.{minutes:00}'{seconds:F2}\"";
+    }
+
+    public static Degrees operator +(Degrees lhs, Degrees rhs) => new(lhs._value + rhs._value);
+
+    public static Degrees operator -(Degrees lhs, Degrees rhs) => new(lhs._value - rhs._value);
+
+    public static Degrees operator *(Degrees lhs, float rhs) => new(lhs._value * rhs);
+
+    public static Degrees operator *(float lhs, Degrees rhs) => new(lhs * rhs._value);
+
+    public static Degrees operator /(Degrees lhs, float rhs) => new(lhs._value / rhs);
+
+    public static Degrees operator /(float lhs, Degrees rhs) => new(lhs / rhs._value);
+
+    public static implicit operator Degrees(float value) => new(value);
 }
